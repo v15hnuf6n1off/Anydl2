47a3cf834cacb7c25f5365083210cb35f83045d7
SayanthD <sayanthd@gmail.com>
2021-01-03 20:57:56 +0530

    Create WORK_DIR whilst initialising

--- anydlbot/__init__.py
+++ anydlbot/__init__.py
@@ -28,6 +28,9 @@ APP_ID = Config.APP_ID
 API_HASH = Config.API_HASH
 AUTH_USERS = filters.user(list(Config.AUTH_USERS))
 WORK_DIR = Config.WORK_DIR
+# create download directory, if not exist
+if not os.path.isdir(WORK_DIR):
+    os.makedirs(WORK_DIR)
 MAX_FILE_SIZE = Config.MAX_FILE_SIZE
 TG_MAX_FILE_SIZE = Config.TG_MAX_FILE_SIZE
 CHUNK_SIZE = Config.CHUNK_SIZE
