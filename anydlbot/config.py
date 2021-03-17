1228afb0857753a6e5a3973e9c3734896d2b1302
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-03-17 23:10:25 +0530

    Add the license and copyright notice on each file

--- anydlbot/config.py
+++ anydlbot/config.py
@@ -1,3 +1,24 @@
+# AnyDLBot - An Open Source ALL-In-One Telegram RoBot
+# Copyright (C) 2018-2021 Shrimadhav U K & Authors
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# This file was referenced and modified after
+# https://github.com/UsergeTeam/Userge-Assistant/blob/master/assistant/config.py
+# which is a part of UsergeTeam/Userge-Assistant project released under the "GNU v3.0 License Agreement".
+# All credit goes to its respective owners
+
 import os
 
 
