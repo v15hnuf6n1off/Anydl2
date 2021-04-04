5245a71db89054721f5c920037c3dda096a0d45c
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-04-04 13:22:44 +0530

    config.py: Replace DEFAULT_THUMBNAIL as former returned 'Certificate expired'

--- anydlbot/config.py
+++ anydlbot/config.py
@@ -38,7 +38,7 @@ class Config:
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 4096))
     PROCESS_MAX_TIMEOUT = int(os.environ.get("PROCESS_MAX_TIMEOUT", 3600))
     DEFAULT_THUMBNAIL = os.environ.get(
-        "DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90"
+        "DEF_THUMB_NAIL_VID_S", "https://via.placeholder.com/320x320.jpg"
     )
     MAX_MESSAGE_LENGTH = int(os.environ.get("MAX_MESSAGE_LENGTH", 4096))
 
