add409bace10033375461eca8e95e8c20c84fb21
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-04-30 13:59:37 +0530

    download_stickers.py: Rewrite with pathlib

--- anydlbot/plugins/download_stickers.py
+++ anydlbot/plugins/download_stickers.py
@@ -14,13 +14,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
-from random import randint
+from pathlib import Path, PurePath
+from tempfile import TemporaryDirectory
 from zipfile import ZipFile
 
 from pyrogram import filters
 from PIL import Image
 
+from anydlbot import LOGGER
 from anydlbot.bot import AnyDLBot
 from anydlbot.config import Config
 from strings import String
@@ -29,52 +30,53 @@ from strings import String
 @AnyDLBot.on_message(filters.sticker & filters.user(Config.USER_IDS))
 async def sticker_downloader(_, message):
     """Copying @DownloadStickersBot"""
+    LOGGER.info(message.from_user)
     sticker = message.sticker
     pack_name = sticker.set_name
     pack_link = f"http://t.me/addstickers/{pack_name}"
-    sticker_ext = os.path.splitext(message.sticker.file_name)[1]
+    sticker_ext = PurePath(message.sticker.file_name).suffix
     zipfile_name = f"{str(message.from_user.id)}_{pack_name}{sticker_ext}.zip"
-    zipfile_path = os.path.join(Config.WORK_DIR, zipfile_name)
-    temp_basename = randint(10000, 99999)
 
-    path_to_download = os.path.join(Config.WORK_DIR, str(temp_basename) + sticker_ext)
-    if not os.path.exists(Config.WORK_DIR):
-        os.makedirs(Config.WORK_DIR)
-
-    status = await message.reply_text(
-        text=String.DOWNLOAD_START, reply_to_message_id=message.message_id
-    )
-    try:
-        download_location = await message.download(
-            file_name=path_to_download, progress_args=String.DOWNLOAD_START
+    Path(Config.WORK_DIR).mkdir(
+        exist_ok=True
+    )  # Cause TemporaryDirectory failed to create parent directory
+    with TemporaryDirectory(dir=Config.WORK_DIR) as tempdir:
+        status = await message.reply_text(
+            text=String.DOWNLOAD_START, reply_to_message_id=message.message_id
         )
-    except ValueError as e:
-        await status.edit_text(text=str(e))
-        return False
-    await status.edit_text(
-        text=String.STICKER_INFO.format(
-            sticker.set_name, sticker.emoji, sticker.file_id
+        try:
+            download_to = await message.download(
+                file_name=f"{tempdir}/", progress_args=String.DOWNLOAD_START
+            )
+        except ValueError as e:
+            await status.edit_text(text=str(e))
+            return False
+        await status.edit_text(
+            text=String.STICKER_INFO.format(
+                sticker.set_name, sticker.emoji, sticker.file_id
+            )
         )
-    )
+        final_location = PurePath(download_to)
+        LOGGER.info(final_location)  # /app/DOWNLOADS/<tempdir>/sticker.webp
 
-    with ZipFile(zipfile_path, "w") as stickerZip:
-        stickerZip.write(download_location, os.path.basename(path_to_download))
+        zipfile_path = (
+            str(final_location.parent) + f"/{zipfile_name}"
+        )  # /app/DOWNLOADS/<tempdir>/<zipfile_name>
+        with ZipFile(zipfile_path, "w") as stickerZip:
+            stickerZip.write(final_location, final_location.name)
 
-    if sticker_ext == ".webp":
-        file_path, ext = os.path.splitext(download_location)
-        png_location = os.path.join(file_path + ".png")
-        # https://stackoverflow.com/a/21669827/4723940
-        Image.open(download_location).convert("RGB").save(png_location, "PNG")
+        if sticker_ext == ".webp":
+            png_location = Path(final_location.with_name(final_location.stem + ".png"))
+            LOGGER.info(png_location)  # /app/DOWNLOADS/<tempdir>/sticker.png
+            # https://stackoverflow.com/a/21669827/4723940
+            Image.open(download_to).convert("RGB").save(png_location, "PNG")
 
-        await status.reply_photo(
-            photo=png_location, reply_to_message_id=status.message_id
+            await status.reply_photo(
+                photo=png_location, reply_to_message_id=status.message_id
+            )
+
+        await status.reply_document(
+            document=zipfile_path,
+            caption=pack_link,
+            reply_to_message_id=status.message_id,
         )
-        os.remove(png_location)
-
-    await status.reply_document(
-        document=zipfile_path, caption=pack_link, reply_to_message_id=status.message_id
-    )
-
-    # Cleanup
-    os.remove(download_location)
-    os.remove(zipfile_path)
