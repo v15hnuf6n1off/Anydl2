38d3e9aa45670c04a24bb23ef39998a6c8907e1b
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-05-03 20:22:56 +0530

    config.py: Add explaination for config vars

--- anydlbot/config.py
+++ anydlbot/config.py
@@ -23,23 +23,27 @@ import os
 
 
 class Config:
+    # Get these two values from my.telegram.org/apps.
     APP_ID = int(os.environ.get("APP_ID", 0))
     API_HASH = os.environ.get("API_HASH")
+    # Create a bot using @BotFather, and get the API token.
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
+    # Directory to handle all downloads
     WORK_DIR = os.environ.get("WORK_DIR", os.path.join(os.getcwd(), "DOWNLOADS"))
-
+    # [Mandatory] The Telegram userid(s) of the separated by a space.
+    # Only the users specified here can use this bot.
     USER_IDS = {os.environ.get("USER_IDS", 0)}
     if os.environ.get("USER_IDS"):
         USER_IDS.update(map(int, os.environ.get("USER_IDS").split()))
     USER_IDS = list(USER_IDS)
-
+    # Specify filesize limit here. Currently 2GB(2000*1024*1024)
     TG_MAX_FILE_SIZE = int(os.environ.get("TG_MAX_FILE_SIZE", 2097152000))
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 4096))
     PROCESS_MAX_TIMEOUT = int(os.environ.get("PROCESS_MAX_TIMEOUT", 3600))
     # DEFAULT_THUMBNAIL = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://via.placeholder.com/320x320.jpg")
     MAX_MESSAGE_LENGTH = int(os.environ.get("MAX_MESSAGE_LENGTH", 4096))
+    # Minimum duration (in seconds)for the video to have for screenshot generation, 15 minutes.
     MIN_DURATION = int(os.environ.get("MIN_DURATION", 900))
-
+    # Specify design for progress block
     FINISHED_PROGRESS_BLOCK = os.environ.get("FINISHED_PROGRESS_BLOCK", "█")
     UNFINISHED_PROGRESS_BLOCK = os.environ.get("UNFINISHED_PROGRESS_BLOCK", "░")
